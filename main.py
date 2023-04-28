bf897313116f3c2b18cb84c820c7cf3b516532fa
MukhammadAyub <47212753+MukhammadAyub@users.noreply.github.com>
2023-04-28 19:57:43 +0500

    Create main.py
    version 1.0

---
+++ main.py
@@ -0,0 +1,8 @@
+from django.conf import settings
+from django.conf.urls.i18n import i18n_patterns
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
+
+from home import views
+from django.utils.translation import gettext_lazy as _
